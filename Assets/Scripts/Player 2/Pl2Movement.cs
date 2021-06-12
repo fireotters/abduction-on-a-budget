fa81a2224e12635a391cf140353ac0dca6c7f5bb
Cameron <cam.crosse97@outlook.com>
2021-06-12 14:35:42 +1000

    Swinging works better
    - Can only choose to swing left OR right if they are still enough
    - If already travelling in a direction, swinging more in that direction is allowed, swinging in opposite direction is disallowed
    --- Player must wait for the counter swing to press the opposite direction
    
    - Added debug text for when the next swing timer is, and what valid inputs are

--- Assets/Scripts/Player 2/Pl2Movement.cs
+++ Assets/Scripts/Player 2/Pl2Movement.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pl2Movement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private float thrust = 500;
+
+    [Header("Plr2")]
+    [SerializeField] private const float ThrustSwing = 500, CooldownBetweenSwings = 1.2f, SwingSwapForgiveness = 0.1f;
+    private float lastSwingTimer = 0f;
+    [SerializeField] private Text textSwingTime;
 
     private void Start()
     {
@@ -14,13 +20,66 @@ public class Pl2Movement : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey("a"))
+        CheckControls();
+    }
+
+    private void CheckControls()
+    {
+        if (Input.GetKeyDown("a") && CanSwing("left"))
         {
-            rb.AddRelativeForce(transform.right * -thrust);
+            rb.AddRelativeForce(transform.right * -ThrustSwing);
         }
-        if (Input.GetKey("d"))
+        if (Input.GetKeyDown("d") && CanSwing("right"))
         {
-            rb.AddRelativeForce(transform.right * thrust);
+            rb.AddRelativeForce(transform.right * ThrustSwing);
         }
+        if (Input.GetKeyDown("w"))
+        {
+
+        }
+        if (Input.GetKeyDown("s"))
+        {
+
+        }
+
+        DebugText();
+    }
+
+    // Player is allowed to swing if:
+    // 1. If player is swinging in the same direction as an input, or is moving at a forgiveable opposite speed.
+    // 2. If player has not swung too recently.
+    private bool CanSwing(string direction)
+    {
+        if (direction == "left" && rb.velocity[0] > SwingSwapForgiveness)
+            return false;
+        if (direction == "right" && rb.velocity[0] < -SwingSwapForgiveness)
+            return false;
+
+        if (Time.time > lastSwingTimer + CooldownBetweenSwings)
+        {
+            {
+                lastSwingTimer = Time.time;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void DebugText()
+    {
+        string swingDirDisplay = "";
+        if (rb.velocity[0] > -SwingSwapForgiveness && rb.velocity[0] < SwingSwapForgiveness)
+            swingDirDisplay = "May press either <-- or -->";
+        else if (rb.velocity[0] > 0)
+            swingDirDisplay = "Can only press -->";
+        else if (rb.velocity[0] < 0)
+            swingDirDisplay = "Can only press <--";
+
+
+
+        double swingTimeDisplay = Math.Round(lastSwingTimer + CooldownBetweenSwings - Time.time, 2);
+        if (swingTimeDisplay < 0)
+            swingTimeDisplay = 0;
+        textSwingTime.text = "Swing timer: " + swingTimeDisplay.ToString() + "\n" + swingDirDisplay;
     }
 }
