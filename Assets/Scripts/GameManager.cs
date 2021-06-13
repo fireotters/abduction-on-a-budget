33addc7ff4369ce6d9b15f97042e09c6378501d6
Cameron <cam.crosse97@outlook.com>
2021-06-14 01:44:45 +1000

    Canvas done, end screen done, done done done help

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,6 @@
 using System;
 using In_Game_Items;
-using UnityEngine.SceneManagement;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,29 +11,19 @@ public class GameManager : MonoBehaviour
 
     private static GameManager _i;
     public static GameManager i { get { if (_i == null) _i = FindObjectOfType<GameManager>(); return _i; } }
-    [SerializeField] private Text keyCountText;
-    [SerializeField] private Text humanCountText;
-    private int _totalCountOfHumans = 0;
+
+    public int totalCountOfHumans = 0;
     
     private void Start()
     {
         var allHumansInCurrentLevel = FindObjectsOfType<Human>();
-        _totalCountOfHumans = allHumansInCurrentLevel.Length;
+        totalCountOfHumans = allHumansInCurrentLevel.Length;
         Invoke(nameof(FindUfo), 0.5f);
     }
 
     private void FindUfo()
     {
         _ufo = FindObjectOfType<Player.UfoController>();
-    }
-
-    private void Update()
-    {
-        if (keyCountText && humanCountText)
-        {
-            keyCountText.text = $"{keyCount}";
-            humanCountText.text = $"{humanCount}/{_totalCountOfHumans}";
-        }
     }
     
     public int keyCount = 0;
