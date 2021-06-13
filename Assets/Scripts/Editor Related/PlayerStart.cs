1df648fddd08bc17b066318f8d2b4fe7e137bb30
Rubén Bermejo Romero <mejoruben@gmail.com>
2021-06-13 10:54:29 +0200

    Created template level for level making Updated player prefab changing position Updated poo water particles to be in right z pos Updated PlayerStart to attach camera to player on level start Fixed MissingAssignmentException on Parallax script

--- Assets/Scripts/Editor Related/PlayerStart.cs
+++ Assets/Scripts/Editor Related/PlayerStart.cs
@@ -1,17 +1,32 @@
+using Cinemachine;
+using Player;
 using UnityEngine;
 
 namespace Editor_Related
 {
     public class PlayerStart : MonoBehaviour
     {
+        private SpriteRenderer _spriteRenderer;
+        private CinemachineVirtualCamera _cinemachineVirtualCamera;
         [SerializeField] private GameObject player = null;
 
-        void Start()
+        private void Awake()
         {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            
+        }
+
+        private void Start()
+        {
+            _cinemachineVirtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
+            _spriteRenderer.enabled = false;
+            
             // Spawns a player.
             if (player != null)
             {
-                Instantiate(player, gameObject.transform.position, Quaternion.identity);
+                var spawnedPlayer = Instantiate(player, gameObject.transform.position, Quaternion.identity);
+                _cinemachineVirtualCamera.m_Follow =
+                    spawnedPlayer.GetComponentInChildren<UfoController>().transform;
             }
             else // This error shouldn't appear, but it's better to be safe than sorry.
             {
